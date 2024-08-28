ccadc02c5b9b7308260856328f7b0171e20fdf62
s-yamauchi07 <hand1nhand306@gmail.com>
2024-08-29 08:44:16 +0900

    Headerの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
+import Header from './Header'
 
-function App() {
+const App = () => {
   return (
-    <div className="text-3xl font-bold underline">トップページ</div>
+    <Header />
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+
+
+const Header = () =>{
+  return(
+    <>
+    <div className="bg-gray-800 p-6">
+      <ul className="flex justify-between">
+        <li><a href="#" className="text-white font-bold">Blog</a></li>
+        <li><a href="#" className="text-white font-bold">お問い合わせ</a></li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default Header
