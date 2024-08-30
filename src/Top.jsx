03669084d528ec298e131e879b376bb994be8f7c
s-yamauchi07 <hand1nhand306@gmail.com>
2024-08-31 08:38:31 +0900

    line-clampの導入

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,36 +1,46 @@
+import { posts } from './data/posts';
+
 
 const Top = () => {
+  const ChangeDateFormat = (date) => date.substring(0, date.indexOf("T")).replace(/-/g, "/");
+  // 改行コードを表示させる
+  const ShortenText = (text) => {
+    const displayText = text.replace(/<br\/>/g, "\n")
+    
+    console.log(displayText)
+    if (text.length >= 55) {
+      return displayText.substring(0, 55) + "..."
+    } else {
+      return text.replace(/<br\/>/g, "\n")
+    }
+  }
+
   return(
     <>
       <div className="m-10">
         <ul className="mx-auto max-w-3xl">
-          <li className="p-4 mb-8 border border-solid border-gray-400">
-            <div className="flex justify-between">
-              <p className="text-gray-400 text-sm">2023/9/11</p>
-              <ul className="flex">
-                <li className="p-1 ml-1 text-blue-600 border border-solid border-blue-600 rounded text-sm">React</li>
-                <li className="p-1 ml-1 text-blue-600 border border-solid border-blue-600 rounded text-sm">TypeScript</li>
-              </ul>
-            </div>
-            <p className="text-2xl my-4">APIで取得した記事タイトル</p>
-            <div>
-              コンテンツ
-            </div>
-          </li>
-
-          <li className="p-4 border border-solid border-gray-400 max-w-3xl">
-            <div className="flex justify-between">
-              <p className="text-gray-400 text-sm">2023/9/11</p>
-              <ul className="flex">
-                <li className="p-1 ml-1 text-blue-600 border border-solid border-blue-600 rounded text-sm">React</li>
-                <li className="p-1 ml-1 text-blue-600 border border-solid border-blue-600 rounded text-sm">TypeScript</li>
-              </ul>
-            </div>
-            <p className="text-2xl my-4">APIで取得した記事タイトル</p>
-            <div>
-              コンテンツ
-            </div>
-          </li>
+            {posts.map((post) => {
+              return(
+                <li key={post.id} className="p-4 mb-8 border border-solid border-gray-400">
+                  <div className="flex justify-between">
+                    <p className="text-gray-400 text-sm">{ChangeDateFormat(post.createdAt)}</p>
+                    <div className="flex">
+                      {post.categories.map((category) => {
+                        return(
+                          <span key={category} className="p-1 ml-1 text-blue-600 border border-solid border-blue-600 rounded text-sm">{category}</span>
+                        )
+                      })}
+                    </div>
+                  </div>
+                  <div className="text-2xl my-4">
+                    <a href="#">{post.title}</a>
+                  </div>
+                  <div className='line-clamp-2'>
+                    {ShortenText(post.content)}
+                  </div>
+                </li>
+              )
+            })}
         </ul>
       </div>
     </>
