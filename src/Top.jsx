acb5d3e9bf746fa2ec2ac623aa2ead96b00ee372
s-yamauchi07 <hand1nhand306@gmail.com>
2024-08-31 17:34:20 +0900

    リンクの設定

--- src/Top.jsx
+++ src/Top.jsx
@@ -10,26 +10,28 @@ const Top = () => {
         <ul className="mx-auto max-w-3xl flex flex-col gap-8">
             {posts.map((post) => {
               return(
-                <li key={post.id} className="p-4 border border-solid border-gray-400">
-                  <div className="flex justify-between">
-                    <p className="text-gray-500 text-xs">{ChangeDateFormat(post.createdAt)}</p>
-                    <div className="flex gap-2">
-                      {post.categories.map((category) => {
-                        return(
-                          <span key={category} className="py-1 px-2 text-blue-600 border border-solid border-blue-600 rounded text-xs">
-                            {category}
-                          </span>
-                        )
-                      })}
+                <a key={post.id} href={`/detail/${post.id}`}>
+                  <li key={post.id} className="p-4 border border-solid border-gray-400">
+                    <div className="flex justify-between">
+                      <p className="text-gray-500 text-xs">{ChangeDateFormat(post.createdAt)}</p>
+                      <div className="flex gap-2">
+                        {post.categories.map((category) => {
+                          return(
+                            <span key={category} className="py-1 px-2 text-blue-600 border border-solid border-blue-600 rounded text-xs">
+                              {category}
+                            </span>
+                          )
+                        })}
+                      </div>
                     </div>
-                  </div>
-                  <div className="text-2xl my-4">
-                    <a href="#">{post.title}</a>
-                  </div>
-                  <div className='line-clamp-2'>
-                    {parse(post.content)}
-                  </div>
-                </li>
+                    <div className="text-2xl my-4">
+                      {post.title}
+                    </div>
+                    <div className='line-clamp-2'>
+                      {parse(post.content)}
+                    </div>
+                  </li>
+                </a>
               )
             })}
         </ul>
